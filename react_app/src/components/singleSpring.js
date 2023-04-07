7c820aa61d9b1a8132128f14b2dc82ba65bbaae8
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2023-04-06 20:07:23 -0400

    like a hotspring btn

--- react_app/src/components/singleSpring.js
+++ react_app/src/components/singleSpring.js
@@ -1,10 +1,19 @@
-const SingleSpring = ({ spring }) => {
+import { useState } from 'react';
+
+const SingleSpring = ({ spring, onItemClick }) => {
+  const [likes, setLikes] = useState(0);
+  
+  const handleLikeBtn = () => {
+    let currentLikes = likes;
+    setLikes(currentLikes += 1);
+  }
+
   return (
     <div className="lg:flex">
       <img 
         className="object-cover w-full h-56 rounded-lg lg:w-64" 
         src="https://tinyurl.com/2p2cfkv2" 
-        alt="hotspring sample" 
+        alt="hotspring sample"
       />
       <div className="flex flex-col justify-between py-6 lg:mx-6">
         <span className="text-xl text-black">
@@ -15,6 +24,17 @@ const SingleSpring = ({ spring }) => {
         </span>
         <p className="text-sm font-semibold text-black">
             {spring.description}
+        </p>
+      </div>
+      <div className="flex py-6 lg:mx-6">
+        <img
+          className="w-14 h-14"
+          src={require("../assets/images/like_icon.png")}
+          alt="like icon"
+          onClick={handleLikeBtn}
+        />
+        <p className="">
+          Likes: {likes}
         </p>
       </div>
     </div>
