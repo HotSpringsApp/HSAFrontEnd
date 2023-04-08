fbb8b87950d5b5b6fff59223e661a7a03069bf7c
Hunter Stevens <hunter@toplobster.io>
2023-04-08 11:50:15 -0500

    mapContainer is operational

--- react_app/src/components/mapContainer.js
+++ react_app/src/components/mapContainer.js
@@ -1,11 +1,47 @@
-const MapContainer = () => {
-  return (
-    <img
-      className=""
-      src={require("../assets/images/map_sample.png")}
-      alt="fake map"
-    />
-  )
-}
+import React from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+// mapboxgl.accessToken = process.env.MAPBOX_KEY;
+// console.log(process.env.MAPBOX_KEY);
 
-export default MapContainer;
+export default class MapContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -122.4,
+      lat: 37.8,
+      zoom: 8,
+    };
+  }
+
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    map.on("move", () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+  }
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+      <div>
+        <div className="sidebarStyle">
+          <div>
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+          </div>
+        </div>
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
+}
