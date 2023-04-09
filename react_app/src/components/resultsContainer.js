dbeb6a55a5f1a3085318c6522ab600f3e09f3af2
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2023-04-09 01:46:55 -0400

    basic modal working when item clicked

--- react_app/src/components/resultsContainer.js
+++ react_app/src/components/resultsContainer.js
@@ -1,16 +1,38 @@
+import { Fragment, useState } from "react";
+import { Button, Dialog, DialogBody, DialogHeader } from "@material-tailwind/react";
+
 import SingleSpring from "./singleSpring";
 
 const ResultsContainer = ({ data }) => {
+  const [displayModal, setDisplayModal] = useState(false);
+
+  const handleClick = () => {
+    setDisplayModal(!displayModal);
+  }
+
   return (
     <>
       <h2 className="text-lg font-bold mb-4 text-center">
         {data.length} results found
       </h2>
       {data.map(spring => 
+      <div>
         <SingleSpring 
           key={spring._id}
           spring={spring}
         />
+        <Fragment>
+          <Button onClick={handleClick} variant="gradient">
+            More info
+          </Button>
+          <Dialog open={displayModal} handler={handleClick}>
+            <DialogHeader>{spring.name}</DialogHeader>
+            <DialogBody divider>
+              {spring.description}
+            </DialogBody>
+          </Dialog>
+        </Fragment>
+      </div>
       )}
     </>
   )
