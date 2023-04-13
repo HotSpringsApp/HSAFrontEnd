c8440fd6838010b8810f172458739f9532550f3e
AlonsoLo <alonso.lobato.jimenez@gmail.com>
2023-04-12 23:19:17 -0400

    click spring displays modal with correct data

--- react_app/src/components/springModal.js
+++ react_app/src/components/springModal.js
@@ -1,10 +1,17 @@
 import { Fragment } from "react";
 import { Dialog, DialogBody, DialogHeader } from "@material-tailwind/react";
+import { useState } from "react";
 
-const SpringModal = ({ spring, openModal }) => {
+const SpringModal = ({ spring, modalState }) => {
+  const [displayModal, setDisplayModal] = useState(modalState);
+
+  const handleClick = () => {
+    setDisplayModal(!displayModal);
+  }
+
   return (
     <Fragment>
-      <Dialog open={openModal}>
+      <Dialog open={displayModal} handler={handleClick}>
         <DialogHeader>{spring.name}</DialogHeader>
         <DialogBody>
           {spring.description}
